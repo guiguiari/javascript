ed24285749afd30d4b07badfb2a6ae7c4bfb8137
guiguiari <45521606+guiguiari@users.noreply.github.com>
2022-04-04 20:37:55 -0300

    a

---
+++ moduloe/repeticao.js
@@ -0,0 +1,24 @@
+var c = 1
+
+while(c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+/*
+do {
+    console.log(`Passo ${c}`)
+    c++ // c + 1
+} while(c <= 6)
+*/
+
+//---------------
+
+
+// ==
+//console.log('Tudo bem?')
+//console.log('Tudo bem?')
+//console.log('Tudo bem?')
+//console.log('Tudo bem?')
+//console.log('Tudo bem?')
+//console.log('Tudo bem?')
