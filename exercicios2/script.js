e9ea17ad80ceac2de21a42131ecc7dbddfdf21fd
guiguiari <45521606+guiguiari@users.noreply.github.com>
2022-04-10 00:07:42 -0300

    a

--- exercicios2/script.js
+++ exercicios2/script.js
@@ -17,3 +17,62 @@ function carregar() {
     }
 }
 
+function verificar() {
+    let data = new Date()
+    let ano = data.getFullYear()
+    let formAno = document.getElementById('ano').value
+    let res = document.getElementById('res')
+    let idadeAtual = ano - formAno
+    let formSex = document.getElementsByName('radsex')
+    let genero = ''
+
+    if (formAno >= 1 && formAno < (ano - 130)) {
+        alert(`Você nasceu no ano ${formAno} e continua vivo? Você tem ${idadeAtual} anos! Parece que você descobriu a fonte da juventude!`)
+    } else if (formAno > ano || formAno == 0) {
+        alert('[ERRO] Digite uma data válida')
+    } else {
+        let criaImg = document.createElement('img')
+        criaImg.setAttribute('id', 'foto')
+
+        if (formSex[0].checked){
+            if (idadeAtual >= 0 && idadeAtual < 10) {
+                criaImg.setAttribute('src', 'imagens/homem-bebe.png')
+                genero = 'Homem'
+            } else if (idadeAtual < 21) {
+                criaImg.setAttribute('src', 'imagens/homem-jovem.png')
+                genero = 'Homem'
+            } else if (idadeAtual < 50) {
+                criaImg.setAttribute('src', 'imagens/homem-adulto.png')
+                genero = 'Homem'
+            } else {
+                criaImg.setAttribute('src', 'imagens/homem-idoso.png')
+                genero = 'Homem'
+            }
+        } else {
+            if (idadeAtual >= 0 && idadeAtual < 10) {
+                genero = 'Mulher'
+                criaImg.setAttribute('src', 'imagens/mulher-bebe.png')
+            } else if (idadeAtual < 21) {
+                criaImg.setAttribute('src', 'imagens/mulher-jovem.png')
+                genero = 'Mulher'
+            } else if (idadeAtual < 50) {
+                criaImg.setAttribute('src', 'imagens/mulher-adulta.png')
+                genero = 'Mulher'
+            } else {
+                criaImg.setAttribute('src', 'imagens/mulher-idosa.png')
+                genero = 'Mulher'
+            }
+        }
+
+        if (formSex[0].checked) {
+            document.body.style.background = '#A7A7FF'
+        } else {
+            document.body.style.background = '#ffb5e6'
+        }
+
+
+        res.innerHTML = `<p>${genero} com ${idadeAtual} anos.</p>`
+        res.appendChild(criaImg)
+
+    }
+}
